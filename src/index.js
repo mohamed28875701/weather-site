56de6568f86f2a0f971b3636d7069fc9d57096c1
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2023-02-23 04:42:49 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import inter from "./interface";
+
+inter.main();
 
 
-    //const fetchResponse=await fetch("https://api.openweathermap.org/data/2.5/weather?q=marieatta&APPID=f9c121c0d80f56fd2c9005d71e216fa8");
     
     
 
